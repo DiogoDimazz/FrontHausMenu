ac7166aa915d4f0d6ebe690daf5c176b7e2a124f
Dimazz <diogo.compositor@gmail.com>
2022-09-01 16:58:38 -0300

    market-section-em-andamento

--- hausmenu/src/Components/ListPage/ListPage.js
+++ hausmenu/src/Components/ListPage/ListPage.js
@@ -1,15 +1,46 @@
 import './styles.css'
 import useConsumer from '../../Hooks/useConsumer'
+import listaDeCompras from './ListaMockada'
+import MarketSection from '../MarketSection/MarketSection'
+import { useEffect, useState } from 'react'
 
 function ListPage() {
     const { groceries, setGroceries } = useConsumer()
+    const [sectionsToGo, setSectionsToGo] = useState([])
 
+    // function cross(e) {
+    //     const grocerie = document.getElementById(e.id)
+    //     grocerie.classList.toggle("grocerie-checked")
+    // }
+
+    function getSections() {
+        const sections = []
+        listaDeCompras.forEach(item => {
+            if (!sections.includes(item.section)) {
+                sections.push(item.section)
+            }
+        })
+
+        setSectionsToGo(sections)
+    }
+
+    useEffect(() => {
+        getSections()
+
+        //eslint-disable-next-line
+    }, [groceries])
 
     return (
         <main className='list-main'>
             <section className='list-notepad'>
-                <div className='list-title'>Lista de Compras - definir fonte</div>
-                <ul className='list-items'></ul>
+                <div className='list-title cursive-text'>Lista de Compras</div>
+                {
+                    sectionsToGo.map((section, index) => (
+                        <div className='list-section' key={index}>
+                            <MarketSection section={section} />
+                        </div>
+                    ))
+                }
             </section>
             <section className='list-filter'>Fazer listagem das seções do supermercado</section>
         </main>
---
+++ hausmenu/src/Components/MarketSection/MarketSection.js
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function MarketSection({ section }) {
+    return (
+        <section className='section-content'>
+            <h5 className='section-title'>{section}</h5>
+        </section>
+    )
+}
